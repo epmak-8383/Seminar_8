adc8f327b8b7fbc62a45b1125ec0f5f3e6a2be0b
Ermachenkov Maxim <epmak.8383@mail.ru>
2022-12-28 00:02:42 +0300

    first

---
+++ Task_1/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задайте двумерный массив. Напишите программу,
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void Falling(int[,] matrix)
+{
+int temp = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+        {
+            if (matrix[i, k] < matrix[i, k + 1])
+            {
+                temp = matrix[i, k + 1];
+                matrix[i, k + 1] = matrix[i, k];
+                matrix[i, k] = temp;
+            }
+        }
+    }
+}
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");          
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateMassive(int rows, int columns, int begin, int end)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(begin, end);            
+        }
+    }
+    return array;
+}
+
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int rows = EnterData("Введите число строк:  ");
+int columns = EnterData("Введите количество столбцов:  ");
+int[,] matrix = CreateMassive(rows, columns, 0, 10);
+PrintArray(matrix);
+Falling(matrix);
+PrintArray(matrix);
